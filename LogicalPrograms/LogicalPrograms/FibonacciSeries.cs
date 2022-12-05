a6a386323d3d8cd48960796ad28eb582bda87548
Nepolean23 <nlean1996@gmail.com>
2022-12-05 22:02:37 +0530

    Code For FibonacciSeries

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void Fibonacci()
+        {
+
+            int val1 = 0;int val2 = 1; int val3;
+            Console.WriteLine("Enter a number for fibonacci series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("0+1");
+            for (int i=2;i<=num;i++)
+            { 
+                val3 = val1 + val2;   
+                Console.Write("+"+val3);
+                val1 = val2;
+                val2 = val3;
+
+            }
+        }
+    }
+}
